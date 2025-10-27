b5268ed5b68c8a01775bde6b87e8381c5ad47c23
s1140350-source <s1140350@dmhs.kh.edu.tw>
2025-10-27 16:23:37 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int age = 80;
+    double height = 180;
+    std::string name = "大於yang";
+    bool is_student = false;
+    std::cout << "名字: "<< name << std::endl;
+    std::cout << "身高: "<< height << std::endl;
+    std::cout << "年齡: "<< age << std::endl;
+    std::cout << "是否為學生: "<< is_student << std::endl;
+    
+
+return 0;
+}
